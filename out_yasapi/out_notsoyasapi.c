4a7952ebf31069160045bbd9f76051abdc7d959d
Darren Owen <winamp.plugins@gmail.com>
2024-09-14 15:54:18 +0100

    Don't update the volume if its a possible query

--- out_yasapi/out_notsoyasapi.c
+++ out_yasapi/out_notsoyasapi.c
@@ -340,7 +340,9 @@ void setvolume(const int volume)
 void setpan(const int pan)
 {
   if (player.options.common.bVolume) {
-    player.base.nPan=pan;
+	if (pan != -666) {
+      player.base.nPan=pan;
+	}
   }
 }
 
